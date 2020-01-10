f24022cb2d0cc157f5877f666a2154c8f5efccb7
Himanshu <guptahimanshu479@gmail.com>
2020-01-10 16:32:04 +0530

    change link

--- public/chat.js
+++ public/chat.js
@@ -1,6 +1,6 @@
 $(function () {
     //make connection
-    var socket = io.connect('http://localhost:8000')
+    var socket = io.connect('https://simple-chatting-app.herokuapp.com/')
 
     //buttons and inputs
     var message = $("#message")
